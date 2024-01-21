0fefc143981cca2c246b5b78de52306f9f08511c
chethancheths <chethancheths13@gmail.com>
2024-01-21 10:02:51 +0530

    binary pyscopg

--- build_files.sh
+++ build_files.sh
@@ -2,9 +2,6 @@
  python3.9 -m venv env
  source env/bin/activate
  echo "Virtual ENV ACTIVATED"
- python3.9 -m pip install pipwin
- pipwin install psycopg2
- echo "psycopg2 INSTALLED"
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
  echo "BUILD END"
