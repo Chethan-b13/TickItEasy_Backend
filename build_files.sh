eaf3cfde7adda689edf6d633ee9bb1958591e884
chethancheths <chethancheths13@gmail.com>
2024-01-21 09:28:28 +0530

    build fix

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,6 @@
  echo "BUILD START"
- python3.12 -m pip install -r requirements.txt
- echo "BUILD END"
+ python3.9 -m pip install pipwin
+ pipwin install psycopg2
+ python3.9 -m pip install -r requirements.txt
+ python3.9 -m pip uninstall pipwin
+ echo "BUILD END"SS
