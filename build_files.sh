0945ca591db057cc40fe71abe7774205033cef7b
chethancheths <chethancheths13@gmail.com>
2024-01-21 10:12:43 +0530

    static

--- build_files.sh
+++ build_files.sh
@@ -3,5 +3,5 @@
  source env/bin/activate
  echo "Virtual ENV ACTIVATED"
  python3.9 -m pip install -r requirements.txt
- python3.9 manage.py collectstatic --noinput --clear
+ python3.9 manage.py collectstatic
  echo "BUILD END"
