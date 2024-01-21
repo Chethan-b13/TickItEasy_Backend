46ecae3bbcc4e97bd0a417bd49067c57277f42e8
chethancheths <chethancheths13@gmail.com>
2024-01-21 09:56:12 +0530

    req and build

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,10 @@
  echo "BUILD START"
  python3.9 -m venv env
  source env/bin/activate
+ echo "Virtual ENV ACTIVATED"
  python3.9 -m pip install pipwin
  pipwin install psycopg2
+ echo "psycopg2 INSTALLED"
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
  echo "BUILD END"
